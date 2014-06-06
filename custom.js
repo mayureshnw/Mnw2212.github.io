7a5e0ebd7171d19cc5c589cc8de87ed438676f06
Mnw2212 <mayuresh2212@gmail.com>
2014-06-06 23:57:30 +0530

    minor changes, added a tag

---
+++ custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("resume").mouseover(function(e) {
+        $(".bg").css("background", "#2094ca");
+    });
+
+    $(".github").mouseover(function(e) {
+        $(".bg").css("background", "#4a535b");
+    });
+
+    $(".email").mouseover(function(e) {
+        $(".bg").css("background", "#DD4B39");
+    });
+
+    $(".linkedin").mouseover(function(e) {
+        $(".bg").css("background", "#0077b5");
+    });
+
+    $(".gplus").mouseover(function(e) {
+        $(".bg").css("background", "#DD4B39");
+  })
+});
